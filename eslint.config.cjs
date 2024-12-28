3e9f71508f39e565b493030585a6c77a8a00c94f
PeterJaberCA <peter@cloudanalyser.com>
2024-12-28 20:02:35 +1100

    init

---
+++ eslint.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    ignores: ['node_modules', 'dist', 'build'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-unused-vars': 'off',
+        'no-console': 'off',
+        'no-empty': 'off',
+    },
+};
